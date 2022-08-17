3956ced8c69238099d61bb4494f9917a37c7661e
louis208908 <lewis8908@gmail.com>
2022-08-17 21:34:39 +0800

    change bias from false to true

--- lab5/src/models/wgan.py
+++ lab5/src/models/wgan.py
@@ -17,27 +17,27 @@ class Generator(nn.Module):
 		)
 		self.main = nn.Sequential(
 			## Input: latent vector z -> convolution
-			nn.ConvTranspose2d(self.args.z_dim + self.args.input_dim * self.args.input_dim, self.args.input_dim * 8, 4, 1, 0, bias=False),
+			nn.ConvTranspose2d(self.args.z_dim + self.args.input_dim * self.args.input_dim, self.args.input_dim * 8, 4, 1, 0, bias=True),
 			nn.BatchNorm2d(self.args.input_dim * 8),
 			nn.ReLU(True),
 
 			## State size: (self.args.input_dim * 8) x 4 x 4
-			nn.ConvTranspose2d(self.args.input_dim * 8, self.args.input_dim * 4, 4, 2, 1, bias=False),
+			nn.ConvTranspose2d(self.args.input_dim * 8, self.args.input_dim * 4, 4, 2, 1, bias=True),
 			nn.BatchNorm2d(self.args.input_dim * 4),
 			nn.ReLU(True),
 			
 			## State size: (self.args.input_dim * 4) x 8 x 8
-			nn.ConvTranspose2d(self.args.input_dim * 4, self.args.input_dim * 2, 4, 2, 1, bias=False),
+			nn.ConvTranspose2d(self.args.input_dim * 4, self.args.input_dim * 2, 4, 2, 1, bias=True),
 			nn.BatchNorm2d(self.args.input_dim * 2),
 			nn.ReLU(True),
 			
 			## State size: (self.args.input_dim * 2) x 16 x 16
-			nn.ConvTranspose2d(self.args.input_dim * 2, self.args.input_dim, 4, 2, 1, bias=False),
+			nn.ConvTranspose2d(self.args.input_dim * 2, self.args.input_dim, 4, 2, 1, bias=True),
 			nn.BatchNorm2d(self.args.input_dim),
 			nn.ReLU(True),
 			
 			## State size: (self.args.input_dim) x 32 x 32
-			nn.ConvTranspose2d(self.args.input_dim, self.args.n_channel, 4, 2, 1, bias=False),
+			nn.ConvTranspose2d(self.args.input_dim, self.args.n_channel, 4, 2, 1, bias=True),
 			nn.Tanh()
 			## State size: (self.args.n_channel) x 64 x 64
 		)
@@ -66,26 +66,26 @@ class Discriminator(nn.Module):
 		)
 		self.main = nn.Sequential(
 			## Input size: (self.args.n_channel + 1) x 64 x 64
-			nn.Conv2d(self.args.n_channel + 1, self.args.input_dim, 4, 2, 1, bias=False),
+			nn.Conv2d(self.args.n_channel + 1, self.args.input_dim, 4, 2, 1, bias=True),
 			nn.LeakyReLU(0.2, inplace=True),
 
 			## State size: (self.args.input_dim) x 32 x 32
-			nn.Conv2d(self.args.input_dim, self.args.input_dim * 2, 4, 2, 1, bias=False),
+			nn.Conv2d(self.args.input_dim, self.args.input_dim * 2, 4, 2, 1, bias=True),
 			nn.InstanceNorm2d(self.args.input_dim * 2, affine=True),
 			nn.LeakyReLU(0.2, inplace=True),
 			
 			## State size: (self.args.input_dim * 2) x 16 x 16
-			nn.Conv2d(self.args.input_dim * 2, self.args.input_dim * 4, 4, 2, 1, bias=False),
+			nn.Conv2d(self.args.input_dim * 2, self.args.input_dim * 4, 4, 2, 1, bias=True),
 			nn.InstanceNorm2d(self.args.input_dim * 4, affine=True),
 			nn.LeakyReLU(0.2, inplace=True),
 			
 			## State size: (self.args.input_dim * 4) x 8 x 8
-			nn.Conv2d(self.args.input_dim * 4, self.args.input_dim * 8, 4, 2, 1, bias=False),
+			nn.Conv2d(self.args.input_dim * 4, self.args.input_dim * 8, 4, 2, 1, bias=True),
 			nn.InstanceNorm2d(self.args.input_dim * 8, affine=True),
 			nn.LeakyReLU(0.2, inplace=True), 
 
 			## State size: (self.args.input_dim * 8) x 4 x 4
-			nn.Conv2d(self.args.input_dim * 8, 1, 4, 1, 0, bias=False), 
+			nn.Conv2d(self.args.input_dim * 8, 1, 4, 1, 0, bias=True), 
 			#nn.Sigmoid()
 		)
 
