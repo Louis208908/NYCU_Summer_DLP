126cc0fd02ab64be7adac2fd89ce069f43f4cde1
louis208908 <lewis8908@gmail.com>
2022-08-18 15:24:42 +0800

    change the model into Dataparallel

--- lab5/src/trainer/trainer.py
+++ lab5/src/trainer/trainer.py
@@ -20,8 +20,7 @@ class Trainer:
 	def __init__(self, args, device, models):
 		self.args = args
 		self.device = device
-
-		self.models = models
+		self.models = nn.DataParallel(models)
 
 		## Create classification model for evaluation
 		self.evaluator = evaluation_model(self.args)
