cb59221f53357334ba0e5334da3bfb9ad4009e3e
louis208908 <lewis8908@gmail.com>
2022-08-25 19:33:55 +0800

    remove a print

--- lab6/ddpg-example.py
+++ lab6/ddpg-example.py
@@ -261,7 +261,7 @@ def train(args, env, agent, writer):
 def test(args, env, agent, writer):
     print('Start Testing')
     # seeds = (args.seed + i for i in range(10))
-    seeds = [6,8,9,12,13,16,17,21,23,33]
+    seeds = [6,8,9,12,13,16,17,23,33,35]
     rewards = []
     for n_episode, seed in enumerate(seeds):
         total_reward = 0
@@ -282,8 +282,8 @@ def test(args, env, agent, writer):
             if done:
                 # writer.add_scalar('Test/Episode Reward', total_reward, n_episode)
                 print('Episode: {}\tTotal reward: {:.2f}'.format(n_episode, total_reward))
-                if total_reward > 290:
-                    print("seed = {}".format(seed))
+                # if total_reward > 290:
+                #     print("seed = {}".format(seed))
                 rewards.append(total_reward)
                 break;
         #         ...
